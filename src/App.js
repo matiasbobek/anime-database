dbcfe2c9ae2688c89750f934162fcac253073e33
matiasbobek <matiasbobek@gmail.com>
2021-05-10 12:36:03 -0300

    Creates the two main pages
    AnimeSearch and AnimeMain. It uses react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import {AnimeSearch} from './components/pages/AnimeSearch'
+import {AnimeMain} from './components/pages/AnimeMain'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <NavLink to="/">Home</NavLink>
+    <Router>
+      <Route path="/" exact>
+        <AnimeSearch/>
+      </Route>
+      <Route path="/anime/:id">
+        <AnimeMain />
+      </Route>
+    </Router>
+    <input value="Search an anime"></input>
+    </>
   );
 }
 
