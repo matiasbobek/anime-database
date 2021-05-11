c8c093b1cde38b4fc5d2379c5ae1489343ac78c2
matiasbobek <matiasbobek@gmail.com>
2021-05-11 19:31:49 -0300

    Adds some styles with bulma

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import AnimeMain  from './components/pages/AnimeMain'
 
 function App() {
   return (
-    <>
+    <div className="container"> 
     <Router>
-      <div className="header">
-        <NavLink to="/">Home</NavLink>
-      </div>
+      <nav class="navbar is-primary" role="navigation" aria-label="main navigation"> 
+        <div id="navbarBasicExample" class="navbar-menu">
+          <div class="navbar-start">
+            <NavLink className="navbar-item" to="/">Home</NavLink>
+          </div>
+        </div>
+      </nav>
       <Route path="/" exact>
         <AnimesSearch/>
       </Route>
@@ -17,7 +21,7 @@ function App() {
         <AnimeMain />
       </Route>
     </Router>
-    </>
+    </div>
   );
 }
 
